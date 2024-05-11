0d45044da48d86e4226ff123949106b4e5631a99
sraj <srajpawar23@yahoo.com>
2024-05-11 16:57:25 +0530

    Test Commit

---
+++ MyTestProject/src/com/demo/test/DemoProject.java
@@ -0,0 +1,11 @@
+package com.demo.test;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DemoTest");
+
+	}
+
+}
